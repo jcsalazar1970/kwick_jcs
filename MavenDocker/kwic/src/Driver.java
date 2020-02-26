9f2a8a8f29cab303a8d0331140bb7d07e6e170c4
juanc <juanc@LAPTOP-5J79I0R8>
2020-02-26 19:36:04 +0100

    2º commit introducimos las palabras no claves desde fichero por hacer el programa más dinámico

--- MavenDocker/kwic/src/Driver.java
+++ MavenDocker/kwic/src/Driver.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import es.uned.master.java.basico.*;
 import es.uned.master.java.coleccion.*;
 
 public class Driver {
 	public static void main(String[] arg){
 		Kwic kwic= new Kwic();
-		String noclaves="a ante cabe con el y y y la contra de por mi las las si segun sobre tras y con a ante con";
+		String nombrefichero = "C:\\kwick\\fichero_cadenas_no_claves.txt";
+	//	String noclaves="a ante cabe con el y y y la contra de por mi las las si segun sobre tras y con a ante con";
+		String noclaves="";
 		String [] frases={
 		                  "El caballero rojo",
 		                  "El reloj de la abadia",
@@ -13,7 +18,24 @@ public class Driver {
 		                  "El abad esta ciego",
 		                  "El AbAd estA CiegO"
 		};
+		
 
+		try {
+			FileReader fr = new FileReader(nombrefichero);
+			BufferedReader br = new BufferedReader(fr);
+			String linea;
+			while((linea = br.readLine()) != null) {
+			//	System.out.println(linea);
+				noclaves += linea ;
+				
+			}
+				
+			fr.close();
+			System.out.println(noclaves);
+		}catch(Exception e) {
+			System.out.println("Excepcion leyendo fichero" + nombrefichero + ": " + e);
+		}
+	/*	
 		kwic.computaNoClaves(noclaves);
 
 		for (int i=0; i<frases.length;i++){
@@ -21,5 +43,6 @@ public class Driver {
 		}
 
 		System.out.println("Kwic:"+kwic.toString());
+		*/
 	}
 }
