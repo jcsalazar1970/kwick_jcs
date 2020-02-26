f348026bf9e4770c47768bffdcc92a3a30bd45ca
juanc <juanc@LAPTOP-5J79I0R8>
2020-02-26 21:17:56 +0100

    3 commit con añadido de entrada de frases desde fichero con Arraylist

--- MavenDocker/kwic/src/Driver.java
+++ MavenDocker/kwic/src/Driver.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 import es.uned.master.java.basico.*;
 import es.uned.master.java.coleccion.*;
@@ -7,10 +8,11 @@ import es.uned.master.java.coleccion.*;
 public class Driver {
 	public static void main(String[] arg){
 		Kwic kwic= new Kwic();
-		String nombrefichero = "C:\\kwick\\fichero_cadenas_no_claves.txt";
+		String nombrefichero_noclave = "C:\\kwick\\fichero_cadenas_no_claves.txt";
+		String nombrefichero_frases = "C:\\kwick\\ficherofrases.txt";
 	//	String noclaves="a ante cabe con el y y y la contra de por mi las las si segun sobre tras y con a ante con";
 		String noclaves="";
-		String [] frases={
+	/*	String [] frases={
 		                  "El caballero rojo",
 		                  "El reloj de la abadia",
 		                  "Dabale arroz a la zorra el abad",
@@ -18,10 +20,12 @@ public class Driver {
 		                  "El abad esta ciego",
 		                  "El AbAd estA CiegO"
 		};
+		*/
+		ArrayList <String> frases = new ArrayList<String>();
 		
-
+     // lectura de palabras no clase desde fichero
 		try {
-			FileReader fr = new FileReader(nombrefichero);
+			FileReader fr = new FileReader(nombrefichero_noclave);
 			BufferedReader br = new BufferedReader(fr);
 			String linea;
 			while((linea = br.readLine()) != null) {
@@ -31,18 +35,39 @@ public class Driver {
 			}
 				
 			fr.close();
-			System.out.println(noclaves);
+			
 		}catch(Exception e) {
-			System.out.println("Excepcion leyendo fichero" + nombrefichero + ": " + e);
+			System.out.println("Excepcion leyendo fichero" + nombrefichero_noclave + ": " + e);
 		}
-	/*	
+		
+		// tratamiento para leer las frases desde fichero en vez desde Array
+		try {
+			FileReader frf = new FileReader(nombrefichero_frases);
+			BufferedReader brf = new BufferedReader(frf);
+			String linea;
+			int i=0;
+			while((linea = brf.readLine()) != null) {
+			
+				frases.add(linea);
+			
+				i++;
+				
+			}
+				
+			frf.close();
+			
+		}catch(Exception e) {
+			System.out.println("Excepcion leyendo fichero" + nombrefichero_frases + ": " + e);
+		}
+		
 		kwic.computaNoClaves(noclaves);
 
-		for (int i=0; i<frases.length;i++){
-			kwic.computaIndice(frases[i]);
+		for (int i=0; i<frases.size();i++){
+			kwic.computaIndice(frases.get(i));
+			
 		}
 
 		System.out.println("Kwic:"+kwic.toString());
-		*/
+		
 	}
 }
