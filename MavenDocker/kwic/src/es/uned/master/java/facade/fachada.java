1b4bca74cadbb9b033377a6d4b3c1e4fe02f8b22
juanc <juanc@LAPTOP-5J79I0R8>
2020-02-28 16:54:46 +0100

    commit con prueba unitaria para ver que ocurre cuando las palabras no claves son igual al conjunto de frases

--- MavenDocker/kwic/src/es/uned/master/java/facade/fachada.java
+++ MavenDocker/kwic/src/es/uned/master/java/facade/fachada.java
@@ -35,6 +35,37 @@ public class fachada {
 		
 	}
 	
+public fachada(String noclaves, String[] frases) {
+	 	
+		this.k = new Kwic();
+		
+		/*
+		 * procedimiento para introducir en private Set<TituloKwic> noclaves;
+		 * las palabras no claves, mediante un parseo con la clase tokenizer de palabras
+		 * separada por espacios o comas, esto introduce en un conjunto de objetos TituloKwi las
+		 * palabras no claves
+		 */
+		this.k.computaNoClaves(noclaves);
+		
+		
+		/*
+		 * bucle sobre el arraylist frases que contiene todas las frases a procesar, dichas se han obtenido previamente
+		 * desde fichero, codigo anterior
+		 * el bucle va recorriendo el arraylist frases, según su tamaño, de ahí 'i<frases.size()'
+		 * como condición de recorrido
+		 * así llama al metodo 'kwick.computaIndice(con la frase a procesar)'
+		 */
+		for (int i=0; i<frases.length;i++){
+			
+			this.k.computaIndice(frases[i]);
+			
+		}
+		
+	}
+	
+	/* 
+	 * metodos para imprimir el kwic
+	 */
 	public void imprimir() {
 		
 		System.out.println("Kwic:"+this.k.toString());
--- MavenDocker/kwic/src/es/uned/master/java/test/kwicvacio.java
+++ MavenDocker/kwic/src/es/uned/master/java/test/kwicvacio.java
@@ -11,6 +11,9 @@ import es.uned.master.java.facade.fachada;
 
 class kwicvacio {
 
+	/*
+	 * prueba unitaria para ver que ocurre si el contenido de las frases es null, al correr la prueba unitaria falla y no funciona
+	 */
 	@Test
 	void test() {
 		String noclaves="a ante cabe con el y y y la contra de por mi las las si segun sobre tras y con a ante con";
